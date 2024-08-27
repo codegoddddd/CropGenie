d54fc6e2a80c716f760ca9c69a38950b52d1d02c
Om Laulkar <130092463+codegoddddd@users.noreply.github.com>
2024-08-27 20:23:28 +0530

    Update app.py - made recommended crop name bold

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ with st.form(key='input_form'):
         crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
 
         crop = crop_dict.get(prediction[0], "Unknown")
-        result = f"{crop} is the best crop to be cultivated with the provided data."
+        result = f"**{crop}** is the best crop to be cultivated with the provided data."
 
         st.subheader('Recommendation')
         st.write(result)
